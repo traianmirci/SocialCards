94585125792b4b0316d3243799c015908594a2fb
Traian Alexandru Mirci <traianmirci@gmail.com>
2018-07-19 18:49:30 +0200

    Añadidas peticiones estándar de un API REST

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ const port = process.env.port || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+
+app.get('/api/usuario', (req,res)=>{
+    res.status(200).send({usuarios: []})
+})
+
+
+app.post('/api/usuario', (req,res)=>{
+    console.log(req.body)    
+    res.status(200).send({message: "Usuario creado correctamente"})
+})
+
+app.put('/api/usuario', (req,res)=>{
+    console.log(req.body)    
+    res.status(200).send({message: "Usuario actualizado correctamente"})
+})
+
+app.delete('/api/usuario', (req,res)=>{
+    console.log(req.body)    
+    res.status(200).send({message: "Usuario borrado correctamente"})
+})
+
+
+
+app.get('*', function(req, res){
+    	res.status(404).send('Petición incorrecta');
+    })
+
 app.listen(port, ()=>{
     console.log(`Corriendo en http://localhost:${port}`)
-})
+    })
