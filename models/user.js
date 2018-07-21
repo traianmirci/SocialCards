58138c4c7c84689353905c45e6195e5add9136ad
Traian Alexandru Mirci <traianmirci@gmail.com>
2018-07-21 17:23:30 +0200

    cifrado de contraseña con bcrypt-nodejs

--- models/user.js
+++ models/user.js
@@ -2,15 +2,36 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
 const UserSchema = new mongoose.Schema({
     name: String,
+    email: {type: String, unique: true, lowercase: true},
+    password: {type: String, select: false},
+    signUpDate: {type: Date, default: Date.now()},
+    lastLogin: Date,
     picture: String,
     biography: String,
     userUrl: String,
     occupation: { type: String, enum: ['Student','Developer']},
     country: { type: String, enum: ['Spain','USA','France']},
     links: [String]
+})
+
+UserSchema.pre('save', (next)=>{
+    let user = this
+    if(!user.isModified('password')) return next()
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, null,(err,hash)=>{
+            if(err) return next(err)
+
+            user.password = hash
+            next()
+        } )
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema)
