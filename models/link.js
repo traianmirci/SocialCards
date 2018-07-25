b02179435322f28b01b81c7c24dc8f00f17d4028
Traian Alexandru Mirci <traianmirci@gmail.com>
2018-07-25 20:36:46 +0200

    Mejorado modelo link

--- models/link.js
+++ models/link.js
@@ -3,12 +3,23 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcrypt-nodejs')
+const User = require('../models/user');
+
 
 const LinkSchema = new mongoose.Schema({
     name: String,
     url: String,
     clicks: [{ visits: Number, date: Date }],
-    active: Boolean
+    type: String, enum: ['facebook', 'twitter', 'instagram','twitch','youtube','tumblr'],
+    active: Boolean,
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+      },
+    publicationdate: {
+    type: Date,
+    default: Date.now()
+    },
 })
 
 
